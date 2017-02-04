5d85d33cf15246949240462c3922d356f73a0164
Kostegan <churilovkonstantin@gmail.com>
2017-02-04 22:31:00 +0400

    chenge springContext and SimpleBeanExample

--- src/main/java/com/test/core/spring/ioc/SimpleBeanExample.java
+++ src/main/java/com/test/core/spring/ioc/SimpleBeanExample.java
@@ -1,6 +1,7 @@
 package com.test.core.spring.ioc;
 
-import com.test.core.spring.beans.Car;
+import com.test.core.spring.ioc.beans.Car;
+import com.test.core.spring.ioc.beans.Garage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -17,8 +18,16 @@ public class SimpleBeanExample {
 
         logger.debug("\n\n\n");
 
-        final Car car = context.getBean("car", Car.class);
-        System.out.println("Car model - " + car.getModel());
-        logger.debug("Car model: {}", car.getModel());
+        final Car car1 = context.getBean("car", Car.class);
+        System.out.println("Car1 model - " + car1.getModel());
+        logger.debug("Car model: {}", car1.getModel());
+        final Car car2 = context.getBean("opelCar",Car.class);
+        System.out.println("Car2 model - " + car2.getModel());
+        logger.debug("Car model: {}", car2.getModel());
+        final Garage garage1 = context.getBean("opelGarage",Garage.class);
+        System.out.println("Garage number is: "+garage1.getNumber());
+        System.out.println("Car in garage number "+garage1.getNumber()+" is "+garage1.getCar().getModel());
+        logger.debug("Garage number is: " + garage1.getNumber());
+        logger.debug("Car in garage number "+garage1.getNumber()+" is "+garage1.getCar().getModel());
     }
 }
